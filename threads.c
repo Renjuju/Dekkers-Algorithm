81c737cfa8c63b5fb01824eb069457c6818715e9
Renju Radhakrishnan <rr553@tux64-12.cs.drexel.edu>
2016-04-30 15:17:07 -0400

    Threads in C, deckers incomplete

---
+++ threads.c
@@ -0,0 +1,32 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <sched.h>
+
+pthread_t tid[2];
+int x = 0;
+
+void* addNumbers() {
+  int i;
+  for(i = 0; i < 100000; i++) {
+    x++;
+  }
+}
+int main() {
+  pthread_attr_t myattr;
+  cpu_set_t cpuset;
+
+  pthread_attr_init(&myattr);
+  CPU_ZERO(&cpuset);
+  CPU_SET(0, &cpuset);
+  pthread_attr_setaffinity_np(&myattr, sizeof(cpu_set_t), &cpuset);
+
+  pthread_create(&(tid[0]), NULL, &addNumbers, NULL);
+  pthread_create(&(tid[1]), NULL, &addNumbers, NULL);
+
+  pthread_join(tid[0], NULL);
+  pthread_join(tid[1], NULL);
+
+  printf("Final output of x should be 200,000\n");
+  printf("x: %i\n", x);
+}
